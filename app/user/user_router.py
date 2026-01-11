f0b097735f4658bafbc708c1d43464a3adf70206
chan-2022 <0chan@yonsei.ac.kr>
2026-01-11 21:20:10 +0900

    Feat: Implement user service, router and update index.html

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -19,16 +19,28 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        new_user = service.register_user(user)
+        return BaseResponse(status="success", data=new_user, message="User Registration Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        deleted_user = service.delete_user(user_delete_request)
+        return BaseResponse(status="success", data=deleted_user, message="User Deletion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    try:
+        updated_user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="Password Update Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- app/user/user_service.py
+++ app/user/user_service.py
@@ -7,21 +7,36 @@ class UserService:
 
     def login(self, user_login: UserLogin) -> User:
         ## TODO
-        user = None
+        user = self.repo.get_user_by_email(user_login.email)
+
+        if user is None:
+            raise ValueError("User not Found.")
+        elif user.password != user_login.password:
+            raise ValueError("Invalid ID/PW")
         return user
         
     def register_user(self, new_user: User) -> User:
         ## TODO
-        new_user = None
-        return new_user
+        user = self.repo.get_user_by_email(new_user.email)
+        if user is not None:
+            raise ValueError("User already Exists.")
+        saved_user = self.repo.save_user(new_user)
+        return saved_user
 
     def delete_user(self, email: str) -> User:
         ## TODO        
-        deleted_user = None
+        deleted_user = self.repo.get_user_by_email(email)
+        if deleted_user is None:
+            raise ValueError("User not Found.")
+        deleted_user = self.repo.delete_user(deleted_user)
         return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
         ## TODO
-        updated_user = None
-        return updated_user
+        existing_user = self.repo.get_user_by_email(user_update.email)
+        if existing_user is None:
+            raise ValueError("User not Found.")
+        existing_user.password = user_update.new_password
+        self.repo.save_user(existing_user)
+        return existing_user
         
