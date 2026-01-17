8216f7aa9d674c5c543788ca63c230d4fe86c795
mhs <hsmoon2013@gmail.com>
2026-01-18 03:05:48 +0900

    add aladin

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -2,10 +2,12 @@ from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
 from review_analysis.crawling.example_crawler import ExampleCrawler
+from review_analysis.crawling.aladin_crawler import AladinCrawler
 
 # 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
     "example": ExampleCrawler,
+    "aladin": AladinCrawler,
 }
 
 def create_parser() -> ArgumentParser:
